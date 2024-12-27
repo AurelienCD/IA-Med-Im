54e8562ef49803cfa8757f4abfb8ec63d5608fa8
Noémie Moreau <163117944+NoemieMr4@users.noreply.github.com>
2024-12-27 12:40:10 +0100

    Add files via upload

--- Classifier/mymodel.py
+++ Classifier/mymodel.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import lightning as L
 from torchmetrics import Accuracy, F1Score
 from torch.optim.lr_scheduler import ReduceLROnPlateau
+import numpy as np
 import timm
 
 class MyModel(L.LightningModule):
@@ -10,15 +11,18 @@ class MyModel(L.LightningModule):
                  lr: float = 0.001,
                  weight_decay: float = 1e-4,
                  num_class: int = 3,
-                 class_counts= [135, 205, 580],
+                 dropout_rate = 0.3,
+                 class_counts= [531, 423, 702],
                  *args,
                  **kwargs):
         super().__init__()
         self.save_hyperparameters()
 
-        total_count = sum(class_counts)
-        class_weights = [total_count / count for count in class_counts]
-        class_weights = torch.tensor(class_weights, dtype=torch.float32)
+        frequencies = np.array(class_counts) / np.array(class_counts).sum()
+        inverse_frequencies = 1 / frequencies
+        weight = inverse_frequencies / inverse_frequencies.max()
+        class_weights = torch.FloatTensor(weight)
+        print(class_weights)
 
         self.model = timm.create_model(
             'resnet51q.ra2_in1k',
@@ -28,16 +32,22 @@ class MyModel(L.LightningModule):
             num_classes=self.hparams.num_class)
 
         in_features = self.model.get_classifier().in_features
-        self.model.fc = nn.Linear(in_features, self.hparams.num_class)
+        self.model.fc = nn.Sequential(
+            nn.Dropout(p=self.hparams.dropout_rate),
+            nn.Linear(in_features, self.hparams.num_class))
 
         self.loss = nn.CrossEntropyLoss(weight=class_weights)
 
-        self.acc = Accuracy(task="multiclass", num_classes=self.hparams.num_class)
+        self.acc_macro = Accuracy(task="multiclass", num_classes=self.hparams.num_class, average='macro')
+        self.acc_micro = Accuracy(task="multiclass", num_classes=self.hparams.num_class, average='micro')
 
         self.f1score = F1Score(task='multiclass', num_classes=self.hparams.num_class, average='weighted')
 
         self.train_labels = []
         self.train_preds = []
+
+        self.validation_step_outputs_preds = []
+        self.validation_step_outputs_targets = []
 
     def forward(self, x):
         return self.model(x)
@@ -48,16 +58,18 @@ class MyModel(L.LightningModule):
     def training_step(self, batch, batch_idx):
         x, y = self.get_input(batch)
 
-        y_hat = self(x)
+        y_hat = self.model(x)
         loss = self.loss(y_hat, y)
         preds = torch.argmax(y_hat, dim=1)
 
-        acc = self.acc(preds, y)
+        acc_macro = self.acc_macro(preds, y)
+        acc_micro = self.acc_micro(preds, y)
 
         f1score = self.f1score(preds, y)
 
         self.log('train/loss', loss, logger=True, batch_size=x.shape[0])
-        self.log('train/acc', acc, logger=True, batch_size=x.shape[0])
+        self.log('train/acc_macro', acc_macro, logger=True, batch_size=x.shape[0])
+        self.log('train/acc_micro', acc_micro, logger=True, batch_size=x.shape[0])
         self.log('train/f1score', f1score, logger=True, batch_size=x.shape[0])
         return loss
 
@@ -68,13 +80,18 @@ class MyModel(L.LightningModule):
         loss = self.loss(y_hat, y)
         preds = torch.argmax(y_hat, dim=1)
 
-        acc = self.acc(preds, y)
+        acc_macro = self.acc_macro(preds, y)
+        acc_micro = self.acc_micro(preds, y)
 
         f1score = self.f1score(preds, y)
 
         self.log('val/loss', loss, logger=True, batch_size=x.shape[0])
-        self.log('val/acc', acc, logger=True, batch_size=x.shape[0])
+        self.log('val/acc_macro', acc_macro, logger=True, batch_size=x.shape[0])
+        self.log('val/acc_micro', acc_micro, logger=True, batch_size=x.shape[0])
         self.log('val/f1score', f1score, logger=True, batch_size=x.shape[0])
+
+        self.validation_step_outputs_preds.extend(preds.cpu().numpy())
+        self.validation_step_outputs_targets.extend(y.cpu().numpy())
         return loss
 
     def test_step(self, batch, batch_idx):
@@ -84,12 +101,14 @@ class MyModel(L.LightningModule):
         loss = self.loss(y_hat, y)
         preds = torch.argmax(y_hat, dim=1)
 
-        acc = self.acc(preds, y)
+        acc_macro = self.acc_macro(preds, y)
+        acc_micro = self.acc_micro(preds, y)
 
         f1score = self.f1score(preds, y)
 
         self.log('test/loss', loss, logger=True, batch_size=x.shape[0])
-        self.log('test/acc', acc, logger=True, batch_size=x.shape[0])
+        self.log('test/acc_macro', acc_macro, logger=True, batch_size=x.shape[0])
+        self.log('test/acc_micro', acc_micro, logger=True, batch_size=x.shape[0])
         self.log('test/f1score', f1score, logger=True, batch_size=x.shape[0])
         return loss
 
--- Classifier/train.py
+++ Classifier/train.py
@@ -1,13 +1,47 @@
 import lightning as L
-import optuna
-from lightning.pytorch.callbacks import ModelCheckpoint, EarlyStopping
+import torch
+import torchmetrics
+from lightning.pytorch.callbacks import ModelCheckpoint, EarlyStopping, Callback
 from lightning.pytorch.loggers import TensorBoardLogger
-from data.MRIDataModule_Alexandre import MRIDataModule
+from data.MRIDataModule_MAJ09122024 import MRIDataModule
 from classifieur.mymodel import MyModel
 import datetime
 import os
+import matplotlib.pyplot as plt
 import pandas as pd
-from lightning.pytorch.strategies import DDPStrategy
+import numpy as np
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from torch.utils.tensorboard import SummaryWriter
+import seaborn as sns
+
+
+class ConfusionMatrixCallback(Callback):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.num_classes = num_classes
+
+    def on_validation_epoch_end(self, trainer, pl_module):
+        # Récupération des prédictions et des cibles depuis le modèle
+        preds = pl_module.validation_step_outputs_preds
+        targets = pl_module.validation_step_outputs_targets
+
+        # Calcul de la matrice de confusion
+        cm = confusion_matrix(targets, preds, labels=np.arange(self.num_classes))
+
+        # Visualisation de la matrice de confusion
+        fig, ax = plt.subplots(figsize=(8, 8))
+        disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=np.arange(self.num_classes))
+        disp.plot(ax=ax, cmap='Blues', values_format='d')
+        plt.title(f"Confusion Matrix - Epoch {trainer.current_epoch}")
+        plt.close(fig)
+
+        # Enregistrement dans TensorBoard
+        trainer.logger.experiment.add_figure(f"Confusion Matrix", fig,
+                                             global_step=trainer.current_epoch)
+
+        # Réinitialiser les listes pour la prochaine époque
+        pl_module.validation_step_outputs_preds = []
+        pl_module.validation_step_outputs_targets = []
 
 def main():
 
@@ -23,8 +57,9 @@ def main():
 
     if logdir is None:  # will be used when there will be an arguments system
         logdir = f'./logs/{now}_{postfixname}'
-        lr = 1.6319144779067916e-05
-        weight_decay = 0.00040378497054698297
+        lr = 0.00024793835138482544
+        weight_decay = 3.5637293220378274e-05
+        dropout_rate = 0.1835202564030761
     else:
         best_hp = pd.read_csv(os.path.join(logdir, 'best_hp.csv'))
         lr = best_hp['lr'][0]
@@ -36,27 +71,29 @@ def main():
 
     patience = 50
 
-    model = MyModel(lr=lr, weight_decay=weight_decay)
+    confusion_matrix_callback = ConfusionMatrixCallback(num_classes=3)
+
+    model = MyModel(lr=lr, weight_decay=weight_decay, dropout_rate=dropout_rate)
 
     checkpoint_callback = ModelCheckpoint(
-        monitor="val/acc",
+        monitor="val/acc_macro",
         dirpath=os.path.join(logdir, 'checkpoints'),
         filename="best_model",
         save_top_k=3,
-        mode="min",
+        mode="max",
     )
 
     early_stopping_callback = EarlyStopping(
-        monitor="val/acc",
+        monitor="val/acc_macro",
         mode="max",
         patience=patience,
         verbose=True
     )
 
-    tensorboard_callback = TensorBoardLogger(name="tensorboard", save_dir=logdir)
+    tensorboard_callback = TensorBoardLogger(name="tensorboard", save_dir=logdir, default_hp_metric="val/acc_macro")
 
     trainer = L.Trainer(
-        callbacks=[checkpoint_callback, early_stopping_callback],
+        callbacks=[checkpoint_callback, early_stopping_callback, confusion_matrix_callback],
         logger=tensorboard_callback,
         max_epochs=n_epoch,
         accelerator='gpu',
@@ -66,23 +103,30 @@ def main():
 
     db_path = '../data'
     task = 'classification'
-    manifest = f'MRI_dataset_{task}_3classes.csv'
+    manifest = f'MRI_dataset_{task}.csv'
+    patient_split = f'patient_splits'
 
     data = MRIDataModule(
         dataset_path=db_path,
         manifest_filename=manifest,
         batch_size=batch_size,
         task=task,
+        num_classes=3,
         crop_size=None,
         train_val_test_shuffle=(True, False, False),
         train_val_test_split=(0.6, 0.2, 0.2),
         weighted_sample=False,
-        seed=23,
+        seed=24,
         verbose=True,
         normalization='max',
-        num_workers=None)
+        num_workers=None,
+        patient_splits_file = patient_split)
+
+    data.setup('fit')  # ou 'test'
+    data.export_dataset_splits()
 
     trainer.fit(model, data)
+
 
 
 if __name__ == '__main__':
